0299d303b4819fee52e32ad8e959e09f305f1d64
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-26 15:43:46 +0100

    added contact page and basic nav ba skeletonsr

---
+++ pages/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="flex items-center justify-between w-full h-[150px] bg-pink-100">
+        <h1 className="text-pink-400 text-5xl ml-24">Nails By Michelle 💅🏼</h1>
+        <ul className="w-80 flex justify-between mr-24 text-pink-400 font-bold text-xl">
+          <li>About</li>
+          <Link href="/price-list">
+            <a>
+              <li>Price List</li>
+            </a>
+          </Link>
+          <Link href="/contact">
+            <a>
+              <li>Contact</li>
+            </a>
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Contact() {
+  return (
+    <div>
+      <h1 className="text-7xl text-blue-400 font-bold">Contact Us</h1>
+      <p>ksjahfl;aksdjvhn;kalsfja;sdfkljnvadflkjanfvdslkj</p>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </div>
+  );
+}
