02019d89bfdeabf9e4aea372f81a08e078e944b8
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-26 15:47:54 +0100

    added price list page and a link back to home - also added a navbar shadow

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -4,10 +4,14 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav>
-      <div className="flex items-center justify-between w-full h-[150px] bg-pink-100">
+      <div className="flex items-center justify-between w-full h-[150px] bg-pink-100 shadow-lg shadow-pink-400">
         <h1 className="text-pink-400 text-5xl ml-24">Nails By Michelle 💅🏼</h1>
         <ul className="w-80 flex justify-between mr-24 text-pink-400 font-bold text-xl">
-          <li>About</li>
+          <Link href="/">
+            <a>
+              <li>About</li>
+            </a>
+          </Link>
           <Link href="/price-list">
             <a>
               <li>Price List</li>
---
+++ pages/price-list.js
@@ -0,0 +1,7 @@
+export default function PriceList() {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold text-blue-600">Price List</h1>
+    </div>
+  );
+}
