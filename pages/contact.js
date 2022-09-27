d4b9079799e14c3de88efe71da2e464f3b3e5eba
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-27 14:57:57 +0100

    padding and spacing for opening hours card changed

--- pages/contact.js
+++ pages/contact.js
@@ -9,62 +9,62 @@ export default function Contact() {
         <link rel="icon" href="/nail-polish.svg" />
       </Head>
       <div className="flex">
-        <div className="flex flex-col items-center w-1/3 h-[400px] mt-12 ml-12 bg-pink-100 rounded-md shadow-lg">
-          <h2 className="text-5xl font-[raleway] flex justify-start text-[#787878]">
+        <div className="p-4 flex flex-col items-center w-1/3 mt-12 ml-12 bg-pink-100 rounded-md shadow-lg">
+          <h2 className="pl-4 text-xl sm:text-4xl md:text-5xl font-[raleway] flex justify-start text-[#787878]">
             Opening Hours
           </h2>
           {/* days and times main div */}
-          <div className="flex flex-col text-left pt-4 pl-4 w-full">
+          <div className="flex flex-col text-left pt-4 pl-4 mr-4 w-full">
             <div className="flex flex-col sm:flex-row">
-              <p className="font-[raleway] font-semibold text-[#787878] text-2xl pr-2 pt-4 w-1/3">
+              <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/3">
                 Monday:
               </p>
-              <p className="font-[raleway] font-semibold text-[#787878] text-2xl pt-4">
+              <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
                 09:00 - 19:00
               </p>
             </div>
 
-            <div className="flex">
-              <p className="font-[raleway] font-semibold text-[#787878] text-2xl pr-2 pt-4 w-1/3">
+            <div className="flex flex-col sm:flex-row">
+              <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/3">
                 Tueday:
               </p>
-              <p className="font-[raleway] font-semibold text-[#787878] text-2xl pt-4">
+              <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
                 09:00 - 19:00
               </p>
             </div>
 
-            <div className="flex">
-              <p className="font-[raleway] font-semibold text-[#787878] text-2xl pr-2 pt-4 w-1/3">
+            <div className="flex flex-col sm:flex-row">
+              <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/3">
                 Wednesday:
               </p>
-              <p className="font-[raleway] font-semibold text-[#787878] text-2xl pt-4">
+              <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
                 09:00 - 19:00
               </p>
             </div>
 
-            <div className="flex">
-              <p className="font-[raleway] font-semibold text-[#787878] text-2xl pr-2 pt-4 w-1/3">
+            <div className="flex flex-col sm:flex-row">
+              <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/3">
                 Thursday:
               </p>
-              <p className="font-[raleway] font-semibold text-[#787878] text-2xl pt-4">
+              <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
                 09:00 - 19:00
               </p>
             </div>
 
-            <div className="flex">
-              <p className="font-[raleway] font-semibold text-[#787878] text-2xl pr-2 pt-4 w-1/3">
+            <div className="flex flex-col sm:flex-row">
+              <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/3">
                 Friday:
               </p>
-              <p className="font-[raleway] font-semibold text-[#787878] text-2xl pt-4">
+              <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
                 09:00 - 19:00
               </p>
             </div>
 
-            <div className="flex">
-              <p className="font-[raleway] font-semibold text-[#787878] text-2xl pr-2 pt-4 w-1/3">
+            <div className="flex flex-col sm:flex-row">
+              <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/3">
                 Saturday:
               </p>
-              <p className="font-[raleway] font-semibold text-[#787878] text-2xl pt-4">
+              <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
                 09:00 - 19:00
               </p>
             </div>
