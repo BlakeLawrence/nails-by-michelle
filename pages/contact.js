45c7f635d156bde973f2fe8c26581a359858fae8
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-27 15:04:43 +0100

    fixed margins

--- pages/contact.js
+++ pages/contact.js
@@ -10,10 +10,11 @@ export default function Contact() {
       </Head>
       <div className="flex">
         <div className="p-4 flex flex-col items-center w-1/3 mt-12 ml-12 bg-pink-100 rounded-md shadow-lg">
-          <h2 className="pl-4 text-xl sm:text-4xl md:text-5xl font-[raleway] flex justify-start text-[#787878]">
+          <h2 className="pl-4 text-xl sm:text-4xl md:text-5xl font-[] flex justify-start text-[#787878]">
             Opening Hours
           </h2>
           {/* days and times main div */}
+
           <div className="flex flex-col text-left pt-4 pl-4 mr-4 w-full">
             <div className="flex flex-col sm:flex-row">
               <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/3">
@@ -29,7 +30,7 @@ export default function Contact() {
                 Tueday:
               </p>
               <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
-                09:00 - 19:00
+                09:00 - 17:00
               </p>
             </div>
 
@@ -38,7 +39,7 @@ export default function Contact() {
                 Wednesday:
               </p>
               <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
-                09:00 - 19:00
+                09:00 - 17:00
               </p>
             </div>
 
@@ -47,7 +48,7 @@ export default function Contact() {
                 Thursday:
               </p>
               <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
-                09:00 - 19:00
+                09:00 - 17:00
               </p>
             </div>
 
@@ -56,7 +57,7 @@ export default function Contact() {
                 Friday:
               </p>
               <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
-                09:00 - 19:00
+                09:00 - 17:00
               </p>
             </div>
 
@@ -65,7 +66,15 @@ export default function Contact() {
                 Saturday:
               </p>
               <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
-                09:00 - 19:00
+                09:30 - 14:00
+              </p>
+            </div>
+            <div className="flex flex-col sm:flex-row">
+              <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/3">
+                Sunday:
+              </p>
+              <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
+                closed
               </p>
             </div>
           </div>
