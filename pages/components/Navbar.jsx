74e23f73d163a4aac7e0bb0c13393ffba749848b
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-28 10:55:41 +0100

    opening hours font changed to montserrat and made alot of improvement on responsiveness

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -12,15 +12,15 @@ const Navbar = () => {
     <>
       {/* Top Nav section - Logo and location etc  */}
       <div className="w-full h-[120px] flex justify-evenly items-center ">
-        <div className="flex items-center">
+        <div className="pl-3 lg:pl-0 flex items-center">
           <Image src={logo} alt="main logo" width={80} height={80}></Image>
-          <h1 className="text-pink-400 text-2xl md:text-3xl lg:text-5xl ml-4 font-[raleway] font-medium">
+          <h1 className="text-pink-400 text-2xl sm:text-3xl lg:text-3xl xl:text-5xl 2xl:text-6xl ml-4 font-[raleway] font-medium">
             Nails By Michelle
           </h1>
         </div>
 
-        <ul className="hidden sm:flex justify-between items-center text-sm sm:text-md">
-          <li className="flex items-center pr-10">
+        <ul className="hidden md:flex justify-between items-center text-sm sm:text-md">
+          <li className="flex items-center pr-4 lg:pr-10">
             <div className="border-[1px] border-gray-300 hover:border-pink-300 rounded-full p-2 mr-3 hover:animate-bounce">
               <ImLocation className="text-pink-400" size="1.5rem" />
             </div>
@@ -29,7 +29,7 @@ const Navbar = () => {
               <p>Cheddar, BS27 3RP</p>
             </div>
           </li>
-          <li className="flex  items-center pr-10">
+          <li className="flex  items-center pr-4 lg:pr-10">
             <div className="border-[1px] border-gray-300 hover:border-pink-300 rounded-full p-2 mr-3 hover:animate-bounce">
               <AiOutlineClockCircle className="text-pink-400" size="1.5rem" />
             </div>
@@ -55,7 +55,7 @@ const Navbar = () => {
         </ul>
       </div>
       {/* Main Nav - menu items (Home, About etc) */}
-      <nav className="w-full h-[70px] flex justify-between items-center bg-pink-200 shadow-pink-400 shadow-md font-[raleway]">
+      <nav className="w-full h-[70px] flex justify-between items-center bg-pink-200 shadow-pink-400 shadow-md font-[montserrat]">
         <ul className="w-full flex justify-around items-center  text-[#868789]">
           <Link href="/">
             <li className="text-sm sm:text-xl font-medium hover:scale-125 hover:cursor-pointer">
--- pages/contact.js
+++ pages/contact.js
@@ -9,72 +9,72 @@ export default function Contact() {
         <link rel="icon" href="/nail-polish.svg" />
       </Head>
       <div className="flex">
-        <div className="p-4 flex flex-col items-center w-full sm:w-1/3 mt-12 ml-4 mr-4 sm:ml-8 bg-pink-100 rounded-md shadow-lg">
-          <h2 className="pl-4 text-xl sm:text-4xl md:text-5xl flex justify-start text-[#787878]">
+        <div className="p-4 flex flex-col items-center w-full md:w-2/3 lg:w-2/3 2xl:w-1/3  mt-12 ml-4 mr-4 sm:ml-8 bg-pink-100 rounded-md shadow-lg">
+          <h2 className="pl-4 text-xl sm:text-4xl md:text-5xl flex justify-start text-[#787878] font-[montserrat]">
             Opening Hours
           </h2>
           {/* days and times main div */}
 
           <div className="flex flex-col text-left pt-4 pl-4 mr-4 w-full">
             <div className="flex sm:flex-row">
-              <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
+              <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
                 Monday:
               </p>
-              <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
+              <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-2xl pt-4">
                 09:00 - 19:00
               </p>
             </div>
 
             <div className="flex sm:flex-row">
-              <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
+              <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
                 Tueday:
               </p>
-              <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
+              <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-2xl pt-4">
                 09:00 - 17:00
               </p>
             </div>
 
             <div className="flex sm:flex-row">
-              <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
+              <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
                 Wednesday:
               </p>
-              <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
+              <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-2xl pt-4">
                 09:00 - 17:00
               </p>
             </div>
 
             <div className="flex sm:flex-row">
-              <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
+              <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
                 Thursday:
               </p>
-              <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
+              <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-2xl pt-4">
                 09:00 - 17:00
               </p>
             </div>
 
             <div className="flex sm:flex-row">
-              <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
+              <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
                 Friday:
               </p>
-              <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
+              <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-2xl pt-4">
                 09:00 - 17:00
               </p>
             </div>
 
             <div className="flex sm:flex-row">
-              <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
+              <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
                 Saturday:
               </p>
-              <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
+              <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-2xl pt-4">
                 09:30 - 14:00
               </p>
             </div>
             <div className="flex sm:flex-row">
-              <p className="font-[raleway] font-semibold text-[#787878] text-lg sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
+              <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
                 Sunday:
               </p>
-              <p className="font-[raleway] font-semibold text-[#787878] text-md sm:text-2xl pt-4">
-                closed
+              <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-2xl pt-4">
+                Closed
               </p>
             </div>
           </div>
