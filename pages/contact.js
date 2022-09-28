1ade45b410320f8e82c8181c8efc5544b3d329dd
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-28 11:13:45 +0100

    changed font size and capitilization for opening hours

--- pages/contact.js
+++ pages/contact.js
@@ -13,69 +13,69 @@ export default function Contact() {
         {/* Opening hourse Card */}
         <div className="flex">
           <div className="p-4 flex flex-col items-center w-full md:w-2/3 lg:w-2/3 2xl:w-1/3  mt-12 ml-4 mr-4 sm:ml-8 bg-pink-100 rounded-md shadow-lg">
-            <h2 className="pl-4 text-xl sm:text-4xl md:text-5xl flex justify-start text-[#787878] font-[montserrat]">
+            <h2 className="pl-4 text-xl sm:text-4xl flex justify-start text-[#787878] font-[montserrat]">
               Opening Hours
             </h2>
             {/* days and times main div */}
             <div className="flex flex-col text-left pt-4 pl-4 mr-4 w-full">
               <div className="flex sm:flex-row">
-                <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
-                  Monday:
+                <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-xl pr-2 pt-4 w-1/2 sm:w-1/3">
+                  MONDAY:
                 </p>
-                <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-2xl 2xl:pl-2 pt-4">
+                <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-xl 2xl:pl-2 pt-4">
                   09:00 - 19:00
                 </p>
               </div>
 
               <div className="flex sm:flex-row">
-                <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
-                  Tueday:
+                <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-xl pr-2 pt-4 w-1/2 sm:w-1/3">
+                  TUESDAY:
                 </p>
-                <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-2xl 2xl:pl-2 pt-4">
+                <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-xl 2xl:pl-2 pt-4">
                   09:00 - 17:00
                 </p>
               </div>
 
               <div className="flex sm:flex-row">
-                <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-2xl pr-3 pt-4 w-1/2 sm:w-1/3">
-                  Wednesday:
+                <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-xl pr-3 pt-4 w-1/2 sm:w-1/3">
+                  WEDNESDAY:
                 </p>
-                <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-2xl 2xl:pl-2 pt-4">
+                <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-xl 2xl:pl-2 pt-4">
                   09:00 - 17:00
                 </p>
               </div>
 
               <div className="flex sm:flex-row">
-                <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
-                  Thursday:
+                <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-xl pr-2 pt-4 w-1/2 sm:w-1/3">
+                  THURSDAY:
                 </p>
-                <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-2xl 2xl:pl-2 pt-4">
+                <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-xl 2xl:pl-2 pt-4">
                   09:00 - 17:00
                 </p>
               </div>
 
               <div className="flex sm:flex-row">
-                <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
-                  Friday:
+                <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-xl pr-2 pt-4 w-1/2 sm:w-1/3">
+                  FRIDAY:
                 </p>
-                <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-2xl 2xl:pl-2 pt-4 2xl:pl-2">
+                <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-xl pt-4 2xl:pl-2">
                   09:00 - 17:00
                 </p>
               </div>
 
               <div className="flex sm:flex-row">
-                <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
-                  Saturday:
+                <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-xl pr-2 pt-4 w-1/2 sm:w-1/3">
+                  SATURDAY:
                 </p>
-                <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-2xl 2xl:pl-2 pt-4">
+                <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-xl 2xl:pl-2 pt-4">
                   09:30 - 14:00
                 </p>
               </div>
               <div className="flex sm:flex-row">
-                <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-2xl pr-2 pt-4 w-1/2 sm:w-1/3">
-                  Sunday:
+                <p className="font-[montserrat] font-semibold text-[#787878] text-xl sm:text-xl pr-2 pt-4 w-1/2 sm:w-1/3">
+                  SUNDAY:
                 </p>
-                <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-2xl 2xl:pl-2 pt-4">
+                <p className="font-[montserrat] font-semibold text-[#787878] text-lg sm:text-xl 2xl:pl-2 pt-4">
                   Closed
                 </p>
               </div>
