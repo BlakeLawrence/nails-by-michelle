baf07755673ae167fb06c8c8639b221b83c35716
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-28 16:20:43 +0100

    made services section responsive

--- pages/components/Services.jsx
+++ pages/components/Services.jsx
@@ -9,20 +9,20 @@ const Services = () => {
     <>
       <div
         id="services"
-        className="flex p-8 pt-12 sm:pt-24 sm:pl-24 bg-[#C084FC]/20"
+        className="flex p-8 pt-12 sm:pt-24 sm:pl-24 bg-[#C084FC]/20 blur-t-sm"
       >
         <h3 className="text-3xl sm:text-5xl lg:text-6xl font-bold font-[montserrat] text-[#f793dc] drop-shadow-lg">
           SERVICES...
         </h3>
       </div>
       <div className="flex justify-center items-center w-full  bg-[#C084FC]/20 pb-24">
-        <div className="w-96 h-96 bg-gray-300/50 mt-36 mr-12 rounded-lg shadow-lg">
+        <div className="w-96 h-32 sm:h-96 bg-[#ffffff] ml-4 sm:ml-0 mt-8 sm:mt-36 mr-4 sm:mr-12 rounded-lg shadow-lg">
           <div className="w-full rounded-t-lg">
             <Image className="rounded-t-lg" src={nails} alt="nails" />
           </div>
         </div>
-        <div className="w-96 h-96 bg-gray-300/50 mt-20 mr-12 rounded-lg  shadow-lg">
-          <div className="w-full h-52 rounded-t-lg bg-[#a75c5c] ">
+        <div className="w-96 h-32 sm:h-96 bg-[#ffffff] mt-2 sm:mt-20 mr-4 sm:mr-12 rounded-lg  shadow-lg">
+          <div className="w-full h-52 rounded-t-lg">
             <Image
               className="rounded-t-lg"
               src={pamper}
@@ -31,8 +31,8 @@ const Services = () => {
             />
           </div>
         </div>
-        <div className="w-96 h-96 bg-gray-300/50 mt-36 mr-12 rounded-lg  shadow-lg">
-          <div className="w-full h-52 rounded-t-lg bg-[#a75c5c]">
+        <div className="w-96 h-32 sm:h-96 bg-[#ffffff] mt-8 sm:mt-36 mr-4 sm:mr-12 rounded-lg  shadow-lg">
+          <div className="w-full h-52 rounded-t-lg ">
             <Image
               className="rounded-t-lg"
               src={lashes}
--- pages/index.js
+++ pages/index.js
@@ -15,14 +15,14 @@ export default function Home() {
         <link rel="icon" href="/nail-polish.svg" />
       </Head>
       {/* Main Hero div */}
-      <div className=" flex w-full bg-[url('/hand.jpg')] h-[400px] sm:h-[550px] bg-no-repeat bg-center bg-cover ">
+      <div className="  flex w-full bg-[url('/hand.jpg')] h-[450px] sm:h-[550px] bg-no-repeat bg-center bg-cover ">
         {/* Heading and buttons div */}
         <div className="flex flex-col p-8 pt-12 sm:p-24 ">
           <h3 className="text-3xl sm:text-5xl lg:text-6xl font-bold font-[montserrat] text-[#f793dc] drop-shadow-lg">
             GO ON, TREAT YOURSELF!
           </h3>
           {/* buttons div */}
-          <div className="mt-6 sm:mt-16">
+          <div className="scroll-smooth mt-6 sm:mt-16">
             <Link href="#services" spy={true} smooth={true} duration={500}>
               <button className="text-[11px] sm:text-[14px] px-4 py-3 mr-6 text-[#ffffff] font-medium font-[montserrat] md:w-32 rounded-full bg-purple-400 hover:scale-110">
                 SERVICES
