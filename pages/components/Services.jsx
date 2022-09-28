9fda7cd54b740e344ae204a7c388c5553f692c11
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-28 16:53:27 +0100

    made services descriptions responsive

--- pages/components/Services.jsx
+++ pages/components/Services.jsx
@@ -17,27 +17,49 @@ const Services = () => {
       </div>
       <div className="flex justify-center items-center w-full  bg-[#C084FC]/20 pb-24">
         <div className="w-96 h-32 sm:h-96 bg-[#ffffff] ml-4 sm:ml-0 mt-8 sm:mt-36 mr-4 sm:mr-12 rounded-lg shadow-lg">
-          <div className="w-full rounded-t-lg">
-            <Image className="rounded-t-lg" src={nails} alt="nails" />
+          <div className="flex flex-col justify-center items-center w-full rounded-t-lg ">
+            <Image
+              className="rounded-t-lg"
+              src={nails}
+              height={1000}
+              alt="nails"
+            />
+            <div className="bg-pink-300 w-2/3 rounded-md mt-6">
+              <p className="flex justify-center text-[11px] sm:text-xl p-4 text-[#ffff] font-medium font-[montserrat]">
+                All Nails
+              </p>
+            </div>
           </div>
         </div>
         <div className="w-96 h-32 sm:h-96 bg-[#ffffff] mt-2 sm:mt-20 mr-4 sm:mr-12 rounded-lg  shadow-lg">
-          <div className="w-full h-52 rounded-t-lg">
+          <div className="flex flex-col justify-center items-center w-full rounded-t-lg ">
             <Image
               className="rounded-t-lg"
               src={pamper}
               height={280}
               alt="pamper party image"
             />
+
+            <div className=" bg-pink-300 w-2/3 rounded-md mt-6">
+              <p className="flex justify-center text-[11px] sm:text-xl p-4 text-[#ffff] font-medium font-[montserrat]">
+                Kids Pamper Parties
+              </p>
+            </div>
           </div>
         </div>
         <div className="w-96 h-32 sm:h-96 bg-[#ffffff] mt-8 sm:mt-36 mr-4 sm:mr-12 rounded-lg  shadow-lg">
-          <div className="w-full h-52 rounded-t-lg ">
+          <div className="flex flex-col justify-center items-center w-full rounded-t-lg ">
             <Image
               className="rounded-t-lg"
               src={lashes}
+              height={825}
               alt="pamper party image"
             />
+            <div className="bg-pink-300 w-2/3 rounded-md mt-6">
+              <p className="flex justify-center text-[11px] sm:text-xl p-4 text-[#ffff] font-medium font-[montserrat]">
+                Lashes - coming soon
+              </p>
+            </div>
           </div>
         </div>
       </div>
