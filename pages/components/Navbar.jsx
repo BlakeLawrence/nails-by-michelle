b0b9014b1b321d0a0565933ef480eb8dcb22c26e
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-29 14:21:55 +0100

    nav color scheme changed

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -46,26 +46,26 @@ const Navbar = () => {
       <div className="w-full h-[120px] flex justify-evenly items-center border-b-[1px] border-gray-200 bg-gradient-to-b ">
         <div className="pl-3 lg:pl-0 flex items-center ">
           <Image src={logo} alt="main logo" width={80} height={80}></Image>
-          <h1 className="text-[#f793dc] text-2xl sm:text-3xl xl:text-4xl ml-4 font-[montserrat] font-medium">
-            Nails By Michelle
+          <h1 className="text-[#fb5dd1] text-2xl sm:text-3xl xl:text-4xl ml-4 font-[montserrat] font-medium">
+            NAILS BY MICHELLE
           </h1>
         </div>
 
         <ul className="hidden sm:flex justify-between items-center text-sm sm:text-md">
           <li className="flex items-center pr-4 lg:pr-10">
-            <div className="border-[1px] border-gray-300 hover:border-pink-300 rounded-full p-2 mr-3 hover:animate-bounce">
+            <div className="border-[1px] border-pink-400 rounded-full p-2 mr-3 hover:animate-bounce">
               <ImLocation className="text-pink-400" size="1.5rem" />
             </div>
-            <div className="flex flex-col text-[10px] md:text-[15px]  text-[#787878] font-[montserrat] font-medium">
+            <div className="flex flex-col text-[10px] md:text-[15px]  text-pink-300 font-[montserrat] font-medium">
               <p>Labourham Way, Draycott</p>
               <p>Cheddar, BS27 3RP</p>
             </div>
           </li>
           <li className="flex  items-center pr-4 lg:pr-10">
-            <div className="border-[1px] border-gray-300 hover:border-pink-300 rounded-full p-2 mr-3 hover:animate-bounce">
+            <div className="border-[1px] border-pink-400 rounded-full p-2 mr-3 hover:animate-bounce">
               <AiOutlineClockCircle className="text-pink-400" size="1.5rem" />
             </div>
-            <div className="flex flex-col text-[10px] md:text-[15px]  text-[#787878] font-[montserrat]">
+            <div className="flex flex-col text-[10px] md:text-[15px]  text-pink-400 font-[montserrat]">
               <p className="font-bold">Mon - Sat</p>
               <Link
                 className="hover:cursor-pointer font-medium"
@@ -76,12 +76,12 @@ const Navbar = () => {
             </div>
           </li>
           <li className="flex  items-center">
-            <div className="border-[1px] border-gray-300 hover:border-pink-300 rounded-full p-2 mr-3 hover:animate-bounce">
+            <div className="border-[1px] border-pink-400 rounded-full p-2 mr-3 hover:animate-bounce">
               <BsFillTelephoneFill className="text-pink-400" size="1.5rem" />
             </div>
-            <div className="flex flex-col text-[10px] md:text-[15px]  text-[#787878] font-[montserrat]">
+            <div className="flex flex-col text-[10px] md:text-[15px]  text-pink-400 font-[montserrat]">
               <p className="font-bold">Call</p>
-              <p className="font-medium">073 081 40607</p>
+              <p className="font-medium text-pink-300">073 081 40607</p>
             </div>
           </li>
         </ul>
@@ -89,27 +89,27 @@ const Navbar = () => {
       <div></div>
       {/* Main Nav - menu items (Home, About etc) */}
       <nav className=" w-full h-[70px] flex justify-between items-center border-b-[1px] border-gray-200 font-[montserrat] ">
-        <ul className="w-full flex justify-around items-center  text-[#868789]">
+        <ul className="w-full flex justify-around items-center  text-pink-400">
           <Link href="/">
-            <li className="text-sm sm:text-xl font-medium hover:scale-125 hover:cursor-pointer  hover:text-pink-400">
+            <li className="text-sm sm:text-xl font-semibold hover:scale-125 hover:cursor-pointer  hover:text-pink-500">
               Home
             </li>
           </Link>
 
           <Link href="/#about">
-            <li className="text-sm sm:text-xl font-medium hover:scale-125 hover:cursor-pointer  hover:text-pink-400">
+            <li className="text-sm sm:text-xl font-semibold hover:scale-125 hover:cursor-pointer  hover:text-pink-500">
               About
             </li>
           </Link>
 
           <Link href="/price-list">
-            <li className="text-sm sm:text-xl font-medium hover:scale-125 hover:cursor-pointer  hover:text-pink-400">
+            <li className="text-sm sm:text-xl font-semibold hover:scale-125 hover:cursor-pointer  hover:text-pink-500">
               Price List
             </li>
           </Link>
 
           <Link href="/contact">
-            <li className="text-sm sm:text-xl font-medium hover:scale-125 hover:cursor-pointer  hover:text-pink-400">
+            <li className="text-sm sm:text-xl font-semibold hover:scale-125 hover:cursor-pointer  hover:text-pink-500">
               Contact
             </li>
           </Link>
