f01afe9bbcc8e19e9e7119564f9d5d5014af717f
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-29 12:36:33 +0100

    chnaged the services section completely

--- pages/components/Services.jsx
+++ pages/components/Services.jsx
@@ -1,71 +1,45 @@
 import React from "react";
-import Image from "next/image";
-import pamper from "../../public/pamper.png";
-import nails from "../../public/nails.jpg";
-import lashes from "../../public/lashes.jpg";
+import { BsHeart } from "react-icons/bs";
 
 const Services = () => {
   return (
     <>
       <div
         id="services"
-        className="flex flex-col p-8 pt-12 sm:pt-24 sm:pl-24 bg-[#C084FC]/20 blur-t-sm"
+        className="p-8 pt-12 sm:pt-24 sm:pl-24 bg-[#C084FC]/20"
       >
-        <h3 className="text-3xl sm:text-5xl lg:text-6xl font-bold font-[montserrat] text-[#f793dc] drop-shadow-lg">
+        <h3 className="text-4xl sm:text-5xl lg:text-6xl font-bold font-[montserrat] text-[#f793dc] drop-shadow-lg">
           SERVICES...
         </h3>
-        <h4 className="pt-12">
-          At current I offer nail services (manicures and pedicures) as well as
-          pamper parties for kids (learn more in the pricing section)
-        </h4>
-      </div>
-      <div className="flex justify-center items-center w-full  bg-[#C084FC]/20 pb-24">
-        <div className="w-96 h-32 sm:h-96 bg-[#ffffff] ml-4 sm:ml-0 mt-8 sm:mt-20 mr-4 sm:mr-12 rounded-lg shadow-lg">
-          <div className="flex flex-col justify-center items-center w-full rounded-t-lg ">
-            <Image
-              className="rounded-t-lg"
-              src={nails}
-              height={1000}
-              alt="nails"
-            />
-            <div className="bg-pink-300 w-11/12 sm:w-2/3 rounded-md mt-2 sm:mt-6">
-              <p className="flex justify-center text-[11px] sm:text-xl p-2 sm:p-4 text-[#ffff] font-medium font-[montserrat]">
-                All Nails
-              </p>
-            </div>
+        <ul className=" pt-12 pb-20">
+          <div>
+            <li className="flex text-2xl sm:text-4xl font-[montserrat] font-bold text-pink-300 pt-4 ">
+              <BsHeart className="mr-6" /> Gel Polish
+            </li>
           </div>
-        </div>
-        <div className="w-96 h-32 sm:h-96 bg-[#ffffff] mt-2 sm:mt-20 mr-4 sm:mr-12 rounded-lg  shadow-lg">
-          <div className="flex flex-col justify-center items-center w-full rounded-t-lg ">
-            <Image
-              className="rounded-t-lg"
-              src={pamper}
-              height={280}
-              alt="pamper party image"
-            />
+          <li className="flex text-2xl sm:text-4xl font-[montserrat] font-bold text-pink-300 pt-4 ">
+            <BsHeart className="mr-6" /> Manicures
+          </li>
+          <li className="flex text-2xl sm:text-4xl font-[montserrat] font-bold text-pink-300 pt-4 ">
+            <BsHeart className="mr-6" /> Pedicures
+          </li>
 
-            <div className=" bg-pink-300 w-11/12 sm:w-2/3 rounded-md mt-2 sm:mt-6">
-              <p className="flex justify-center text-[11px] sm:text-xl p-2 sm:p-4 text-[#ffff] font-medium font-[montserrat]">
-                Kids Pamper Parties
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="w-96 h-32 sm:h-96 bg-[#ffffff] mt-8 sm:mt-20 mr-4 sm:mr-12 rounded-lg  shadow-lg">
-          <div className="flex flex-col justify-center items-center w-full rounded-t-lg ">
-            <Image
-              className="rounded-t-lg"
-              src={lashes}
-              height={825}
-              alt="pamper party image"
-            />
-            <div className="bg-pink-300 w-11/12 sm:w-2/3 rounded-md mt-2 sm:mt-6">
-              <p className="flex justify-center text-[11px] sm:text-xl p-2 sm:p-4 text-[#ffff] font-medium font-[montserrat]">
-                Lashes
-              </p>
-            </div>
-          </div>
-        </div>
+          <li className="flex text-2xl sm:text-4xl font-[montserrat] font-bold text-pink-300 pt-4 ">
+            <BsHeart className="mr-6" /> Gel Extensions
+          </li>
+          <li className="flex text-2xl sm:text-4xl font-[montserrat] font-bold text-pink-300 pt-4 ">
+            <BsHeart className="mr-6" /> Acrylic Extensions
+          </li>
+          <li className="flex text-2xl sm:text-4xl font-[montserrat] font-bold text-pink-300 pt-4 ">
+            <BsHeart className="mr-6" /> Acrylic Overlays
+          </li>
+          <li className="flex text-2xl sm:text-4xl font-[montserrat] font-bold text-pink-300 pt-4 ">
+            <BsHeart className="mr-6" /> Acrylic Infill
+          </li>
+          <li className="flex text-2xl sm:text-4xl font-[montserrat] font-bold text-pink-300 pt-4 ">
+            <BsHeart className="mr-6" /> Eye Lash extensions
+          </li>
+        </ul>
       </div>
     </>
   );
