9d9529dc3b6ef2790da8dc867b1fea35404b165b
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-30 14:48:57 +0100

    price liost font resposniveness done

--- pages/components/PriceItem.jsx
+++ pages/components/PriceItem.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const PriceItem = ({ price }) => {
   return (
-    <div className="pt-8 flex items-center justify-between">
-      <li className="main-welcome font-[montserrat] text-xl sm:text-4xl text-pink-400 font-boldpl-0 lg:pl-8">
+    <div className="pt-8 flex items-center justify-between bg-gray-100 mt-3 pb-3 rounded-xl hover:shadow-md hover:scale-105">
+      <li className="pb-4 main-welcome font-[montserrat] text-xl sm:text-3xl md:text-4xl text-pink-400 font-semibold  pl-2 sm:pl-3 lg:pl-8 ">
         {price.name}
       </li>
-      <div className="flex justify-evenly">
-        <div className="sm:w-40 flex justify-between items-center">
-          <p className="main-welcome font-[montserrat] text-xl pr-4 text-pink-400 font-bold">
+      <div className="flex justify-evenly ">
+        <div className="sm:w-40 flex justify-between items-center ">
+          <p className="main-welcome font-[montserrat] text-xl pr-4 text-pink-400 font-medium">
             -
           </p>
-          <p className="main-welcome font-[montserrat] text-xl sm:text-3xl pr-6 sm:pr-12 text-pink-400 font-bold">
+          <p className="main-welcome font-[montserrat] text-xl sm:text-3xl pr-6 sm:pr-12 text-pink-400 font-semibold">
             £{price.price}
           </p>
         </div>
