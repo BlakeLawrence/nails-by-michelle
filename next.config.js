2ff7e2a7b7f21270ba61ecaf924fcf6b0401c0f8
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-30 16:32:59 +0100

    changed next config file toi help deploy

--- next.config.js
+++ next.config.js
@@ -4,14 +4,4 @@ const nextConfig = {
   swcMinify: true,
 };
 
-module.exports = {
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    mySecret: "secret",
-    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    staticFolder: "/static",
-  },
-};
+module.exports = nextConfig;
