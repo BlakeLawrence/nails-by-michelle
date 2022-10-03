fab624dd53473bba120fb3f1e95c5b3d2433926e
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-10-03 11:55:28 +0100

    added further photo gallery responsiveness

--- components/ListItem.jsx
+++ components/ListItem.jsx
@@ -3,7 +3,7 @@ import { BsHeart } from "react-icons/bs";
 
 const ListItem = ({ service }) => {
   return (
-    <li className="main-welcome flex text-[20px] sm:text-[26px] md:text-4xl font-bold text-[#fa9bcf] pt-4 mr-16 sm:mr-24 sm:pr-0">
+    <li className="main-welcome flex text-[20px] sm:text-[26px] md:text-4xl font-bold text-[#fa9bcf] pt-4 mr-10 sm:mr-24 sm:pr-0 container">
       <BsHeart className="mr-2 sm:mr-6 hover:scale-125 w-4 sm:w-6" />
       {service}
     </li>
