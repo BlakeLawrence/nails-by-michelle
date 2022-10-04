cf8817920ae49079b7a5ec9e8ab57de26578b805
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-10-04 14:55:55 +0100

    contact page responsive

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Footer from "../components/Footer";
-import { FaFacebook, FaInstagram } from "react-icons/fa";
+import Link from "next/link";
 import { BsFillTelephoneFill, BsWhatsapp } from "react-icons/bs";
 import { AiOutlineMail } from "react-icons/ai";
 
@@ -15,7 +15,7 @@ export default function Contact() {
       {/* main contact card */}
       <div className="flex flex-col h-screen w-full">
         <div className="flex flex-col items-center w-full h-1/3 bg-pink-300 pt-12 lg:pt-8 text-gray-100 font-bold">
-          <h3 className="main-welcome text-6xl md:text-7xl">Get in touch...</h3>
+          <h3 className="main-welcome text-6xl lg:text-7xl">Get in touch...</h3>
         </div>
         {/* Contact Icons */}
         <div className="flex flex-col md:flex-row w-full h-2/3 bg-gray-100">
@@ -24,80 +24,82 @@ export default function Contact() {
               <ul className="p-4 text-pink-400 font-bold">
                 <li className=" flex mt-5 md:mt-8">
                   <BsFillTelephoneFill className="w-[1.5rem] h-[1.5rem] md:w-[1.8rem] md:h-[1.8rem]" />
-                  <p className="pl-4 flex items-center text-[14px]">
+                  <p className="pl-4 flex items-center text-[13px] sm:text-[14px] font-[montserrat]">
                     0791 768 2549
                   </p>
                 </li>
                 <li className="flex mt-5 md:mt-8 ">
                   <BsWhatsapp className="w-[1.5rem] h-[1.5rem] md:w-[2rem] md:h-[2rem]" />
-                  <p className="pl-4 flex items-center text-[14px] md:text-sm">
+                  <p className="pl-4 flex items-center text-[13px] sm:text-[14px] md:text-sm font-[montserrat]">
                     WhatsApp
                   </p>
                 </li>
-                <li className="flex mt-5 md:mt-8">
-                  <AiOutlineMail className="w-[1.5rem] h-[1.5rem] md:w-[2rem] md:h-[2rem]" />
-                  <p className="pl-4 flex items-center text-[14px] md:text-sm">
-                    Email
-                  </p>
-                </li>
+                <Link href="mailto:blake-lawrence@hotmail.co.uk">
+                  <li className="flex mt-5 md:mt-8 hover:cursor-pointer">
+                    <AiOutlineMail className="w-[1.5rem] h-[1.5rem] md:w-[2rem] md:h-[2rem]" />
+                    <p className="pl-4 flex items-center text-[13px] sm:text-[14px] md:text-sm font-[montserrat]">
+                      Email
+                    </p>
+                  </li>
+                </Link>
               </ul>
             </div>
             {/* Opening Hours */}
             <div className=" flex justify-center md:justify-start w-8/12 lg:w-7/12 h-[200px] md:h-[300px] bg-gray-100 border-[1px] border-pink-300 mt-[-70px] lg:mt-[-90px] mr-2 ml-1 lg:ml-8 rounded-xl">
               <ul className="p-2  text-pink-400 font-bold">
-                <li className=" flex justify-between mt-1 md:mt-3 lg:pl-2 text-[12px] md:text-md">
-                  <p className=" flex items-center text-[12px] md:text-[14px]">
+                <li className=" flex justify-between mt-1 md:mt-3 lg:pl-2 text-[12px] md:text-md font-[montserrat]">
+                  <p className=" flex items-center text-[11px] md:text-[14px] ">
                     Monday:
                   </p>
-                  <p className="pl-4 flex items-center text-[11px] md:text-[13px]">
+                  <p className="pl-4 flex items-center text-[10px] md:text-[13px] ">
                     09:00 - 19:00
                   </p>
                 </li>
-                <li className=" flex justify-between mt-1 md:mt-3 lg:pl-2 text-[12px] md:text-md">
-                  <p className=" flex items-center text-[12px] md:text-[14px]">
+                <li className=" flex justify-between mt-1 md:mt-3 lg:pl-2 text-[12px] md:text-md font-[montserrat]">
+                  <p className=" flex items-center text-[11px] md:text-[14px]">
                     Tuesday:
                   </p>
-                  <p className="pl-4 flex items-center text-[11px] md:text-[13px]">
+                  <p className="pl-4 flex items-center text-[10px] md:text-[13px]">
                     09:00 - 17:00
                   </p>
                 </li>
-                <li className=" flex justify-between mt-1 md:mt-3 lg:pl-2 text-[12px] md:text-md">
-                  <p className=" flex items-center text-[12px] md:text-[14px]">
+                <li className=" flex justify-between mt-1 md:mt-3 lg:pl-2 text-[12px] md:text-md font-[montserrat]">
+                  <p className=" flex items-center text-[11px] md:text-[14px]">
                     Wednesday:
                   </p>
-                  <p className="pl-4 flex items-center text-[11px] md:text-[13px]">
+                  <p className="pl-4 flex items-center text-[10px] md:text-[13px]">
                     09:00 - 17:00
                   </p>
                 </li>
-                <li className=" flex justify-between mt-1 md:mt-3 lg:pl-2 text-[12px] md:text-md">
-                  <p className=" flex items-center text-[12px] md:text-[14px]">
+                <li className=" flex justify-between mt-1 md:mt-3 lg:pl-2 text-[12px] md:text-md font-[montserrat]">
+                  <p className=" flex items-center text-[11px] md:text-[14px]">
                     Thursday:
                   </p>
-                  <p className="pl-4 flex items-center text-[11px] md:text-[13px]">
+                  <p className="pl-4 flex items-center text-[10px] md:text-[13px]">
                     09:00 - 17:00
                   </p>
                 </li>
-                <li className=" flex justify-between mt-1 md:mt-3 lg:pl-2 text-[12px] md:text-md">
-                  <p className=" flex items-center text-[12px] md:text-[14px]">
+                <li className=" flex justify-between mt-1 md:mt-3 lg:pl-2 text-[12px] md:text-md font-[montserrat]">
+                  <p className=" flex items-center text-[11px] md:text-[14px]">
                     Friday:
                   </p>
-                  <p className="pl-4 flex items-center text-[11px] md:text-[13px]">
+                  <p className="pl-4 flex items-center text-[10px] md:text-[13px]">
                     09:00 - 17:00
                   </p>
                 </li>
-                <li className=" flex justify-between mt-1 md:mt-3 lg:pl-2 text-[12px] md:text-md">
-                  <p className=" flex items-center text-[12px] md:text-[14px]">
+                <li className=" flex justify-between mt-1 md:mt-3 lg:pl-2 text-[12px] md:text-md font-[montserrat]">
+                  <p className=" flex items-center text-[11px] md:text-[14px]">
                     Saturday:
                   </p>
-                  <p className="pl-4 flex items-center text-[11px] md:text-[13px]">
+                  <p className="pl-4 flex items-center text-[10px] md:text-[13px]">
                     09:30 - 14:00
                   </p>
                 </li>
-                <li className=" flex justify-between mt-1 md:mt-3 lg:pl-2 text-[12px] md:text-md">
-                  <p className=" flex items-center text-[12px] md:text-[15px]">
+                <li className=" flex justify-between mt-1 md:mt-3 lg:pl-2 text-[12px] md:text-md font-[montserrat]">
+                  <p className=" flex items-center text-[11px] md:text-[15px]">
                     Sunday:
                   </p>
-                  <p className=" flex items-center text-[12px] md:text-[14px]">
+                  <p className=" flex items-center text-[11px] md:text-[14px]">
                     Closed
                   </p>
                 </li>
@@ -106,10 +108,10 @@ export default function Contact() {
           </div>
           {/* Thank you paragraph */}
           <div className="w-full lg:w-2/5 flex flex-col ">
-            <div className=" text-center pt-12 text-xl md:text-3xl font-medium font-[montserrat] text-pink-400 ">
+            <div className=" text-center pt-12 text-xl md:text-3xl font-medium font-[montserrat] text-pink-400  ">
               Thank you for popping in...
             </div>
-            <div className=" text-center ml-2 pt-6 text-md md:text-xl font-medium font-[montserrat] text-pink-400 ">
+            <div className=" text-center ml-2 pt-6 text-md md:text-xl font-medium font-[montserrat] text-pink-400  ">
               For the quickest response, please whatsapp or call
             </div>
           </div>
