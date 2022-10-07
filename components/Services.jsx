1b4f6284a0c20f259a31b3a701ce5bc068f17632
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-10-07 09:47:58 +0100

    changed photo layout

--- components/Services.jsx
+++ components/Services.jsx
@@ -8,19 +8,19 @@ const Services = () => {
     <>
       <div
         id="services"
-        className="pt-6 pl-4 w-full pb-6 pr-2 sm:pt-8 xl:pl-20 bg-[#C084FC]/20 border-t-[1px] border-pink-200 "
+        className="pt-6 pl-4 w-full pb-6 pr-4 sm:pt-8 xl:pl-20 bg-[#C084FC]/20 border-t-[1px] border-pink-200 "
       >
         <h3 className="main-welcome text-4xl sm:text-5xl lg:text-6xl font-bold  text-[#f793dc] drop-shadow-lg">
           Services...
         </h3>
-        <div className="flex justify-between">
+        <div className="flex flex-col sm:flex-row justify-between">
           <ul className="w-2/3 pt-12 pb-2">
             {services.map((service, index) => (
               <ListItem key={index} service={service} />
             ))}
             <div className="pt-6 md:pt-12">
               <Link href="/price-list">
-                <button className="text-[11px] sm:text-[16px] py-3 bg-purple-400 border-[1px] font-semibold font-[montserrat] w-20 md:w-28 border-purple-400 rounded-full text-[#ffffff] hover:scale-110 ease-in duration-100 mb-12">
+                <button className="text-[11px] sm:text-[16px] py-3 bg-purple-400 border-[1px] font-semibold font-[montserrat] w-20 md:w-28 border-purple-400 rounded-full text-[#ffffff] hover:scale-110 ease-in duration-100 mb-8 sm:mb-12">
                   Price List
                 </button>
               </Link>
