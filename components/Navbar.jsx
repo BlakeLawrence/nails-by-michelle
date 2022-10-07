04d049d1b4c8a27fca2a5157f6161a0d527b3cc3
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-10-07 16:38:28 +0100

    changed nav bg back to white

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <>
       {/* location etc only for phone mode */}
       <div role="navigation">
-        <ul className="sm:hidden flex justify-between items-center text-[9px] p-2 border-b-[1px] border-pink-200 bg-[#fffff3] ">
+        <ul className="sm:hidden flex justify-between items-center text-[9px] p-2 border-b-[1px] border-pink-200 bg-[#ffffff] ">
           <li className="flex items-center pr-2">
             <div className="border-[1px] border-pink-400 hover:border-pink-300 rounded-full p-2 mr-3">
               <ImLocation className="text-pink-400" size="1rem" />
@@ -51,7 +51,7 @@ const Navbar = () => {
         </ul>
       </div>
       {/* Main Nav section - Logo and location etc  */}
-      <nav className="w-full h-[120px] flex justify-between items-center border-b-[1px] border-pink-200 bg-[#fffff3] ">
+      <nav className="w-full h-[120px] flex justify-between items-center border-b-[1px] border-pink-200 bg-[#ffffff] ">
         <div className=" flex pt-2 sm:pt-1 ml-2 md:ml-4">
           <Image
             src={logo}
@@ -101,7 +101,7 @@ const Navbar = () => {
       </nav>
 
       {/* Main Nav - menu items (Home, About etc) */}
-      <nav className=" w-full h-[70px] flex justify-between items-center border-b-[3px] border-b-pink-200 font-[montserrat] bg-[#fffff3] ">
+      <nav className=" w-full h-[70px] flex justify-between items-center border-b-[3px] border-b-pink-200 font-[montserrat] bg-[#ffffff] ">
         <ul className="w-full flex justify-around items-center  text-pink-400">
           <Link href="/">
             <li className="main-welcome text-md sm:text-2xl font-semibold hover:scale-125 ease-in duration-100 hover:cursor-pointer  hover:text-pink-500">
