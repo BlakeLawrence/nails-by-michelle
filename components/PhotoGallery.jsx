2278720d3600045e369bf9f990e0d8ba5d494b50
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2024-04-06 09:00:32 +0100

    changed pics on home page

--- components/PhotoGallery.jsx
+++ components/PhotoGallery.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Image from "next/image";
 
-import nails1 from "../public/1-nails.jpeg";
-import nails3 from "../public/3-nails.jpeg";
-import nails4 from "../public/4-nails.jpeg";
-import nails5 from "../public/5-nails.jpeg";
-import lashes1 from "../public/lashes1.jpg";
-import lashes2 from "../public/lashes2.jpg";
+import nails1 from "../public/1-nails.jpg";
+import nails3 from "../public/3-nails.jpg";
+import nails4 from "../public/4-nails.jpg";
+import nails5 from "../public/5-nails.jpg";
 
 const PhotoGallery = () => {
   return (
